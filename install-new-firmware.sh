f10829e5459e0b51cf8b8aac006926d0296e2beb
David Kagerer <david@binder-kagerer.de>
2017-07-09 00:31:58 +0200

    Update install-new-firmware.sh

--- install-new-firmware.sh
+++ install-new-firmware.sh
@@ -1,15 +1,17 @@
-git pull origin printers/Anet-A8
 
 git submodule init
 git submodule update
 
 # copy configs into the right folder
 cd Firmware/Marlin/Marlin
+git pull origin printers/default
+
 rm -rf Configuration.h
 rm -rf Configuration_adv.h
 cp my_configuration_files/Anet-A8/Configuration.h .
 cp my_configuration_files/Anet-A8/Configuration_adv.h .
 cd ..
+
 # compile and upload
 pio run -t upload
 
